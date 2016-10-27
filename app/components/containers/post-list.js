c19a76839290d5b8853f2e67c56c065dd71d51ea
Noa Fisher <Noa Fisher>
2016-10-27 16:11:29 +0300

    final pager?

--- app/components/containers/post-list.js
+++ app/components/containers/post-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { Link } from 'react-router';
+import Pager from '../pager';
 
 
 class PostList extends React.Component {
@@ -18,7 +18,7 @@ class PostList extends React.Component {
     let date = `${day} ${month}, ${year}`;
     return date;
   }
-  extractPage(posts, nextPage){
+  extractVisiblePosts(posts, nextPage){
     const length = posts.length;
     let start = nextPage === 1 ? 0 : nextPage * 3 - 3;
     let end = length % 3 === length - start ?  length  : start + 3;
@@ -28,7 +28,7 @@ class PostList extends React.Component {
 
   render() {
     let { posts, nextPage } = this.props;
-    const visiblePosts = this.extractPage(posts, nextPage);
+    const visiblePosts = this.extractVisiblePosts(posts, nextPage);
     return (
       <section className="col-md-8">
         <h2 className="page-header">Showing 8 posts</h2>
@@ -72,14 +72,7 @@ class PostList extends React.Component {
           );
         })
         }
-        <ul className="pager">
-          {+nextPage > 1 ? <li className="previous">
-                             <Link to={`/posts/${ +nextPage - 1}`}>← Older</Link>
-                          </li> : null}
-          {+nextPage < Math.ceil(posts.length / 3 )? <li className="next">
-                            <Link to={`/posts/${ +nextPage + 1}`}>Newer →</Link>
-                           </li> : null}
-        </ul>
+        <Pager nextPage={+nextPage} postsLength={posts.length}/>
       </section>
     );
   }
---
+++ app/components/pager.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class Pager extends React.Component {
+
+  renderOlderBtn(nextPage) {
+    if (nextPage > 2) {
+      return <Link to={`/posts/${ nextPage - 1}`}>← Older</Link>
+    }
+    else if (nextPage === 2) {
+      return <Link to={`/posts/`}>← Older</Link>
+    }
+    else {
+      return null;
+    }
+  }
+
+  render() {
+    let {nextPage, postsLength} = this.props;
+    return (
+      <ul className="pager">
+        <li className="previous">
+          {this.renderOlderBtn(nextPage)}
+        </li>
+        {nextPage < Math.ceil(postsLength / 3) ? <li className="next">
+          <Link to={`/posts/${ nextPage + 1}`}>Newer →</Link>
+        </li> : null}
+      </ul>
+    );
+  }
+}
+export default Pager;
