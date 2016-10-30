2d79dc761b3a1a4f3a5010ee4f5ffc3c611821b2
Noa Fisher <Noa Fisher>
2016-10-30 05:37:39 +0200

    side bar init

--- app/components/side-bar.js
+++ app/components/side-bar.js
@@ -1,103 +1,126 @@
 import React from 'react';
+import { connect } from  'react-redux';
+import { Link } from  'react-router';
 
-const SideBar = (props) => {
-  return (
-  <aside className="col-md-4">
-    {/* Blog Search Well */}
-    <div className="well">
-      <h4>Search</h4>
-      <form>
-        <div className="input-group">
-          <input type="search" name="search" className="form-control"/>
+class SideBar extends React.Component {
+  extractArray(arr, filter){
+    let extracted = arr.map(item => {
+      return item[filter];
+    });
+    return extracted;
+  }
+  flattenDeep(arr){
+    let spread = arr.reduce((acc,item) => {
+      return [...acc,...item];
+    },[]);
+    return spread;
+  }
+  dateToMonthYear(arr){
+    let monthYear = arr.map(item => {
+      let [,month,,year] = new Date(+item).toDateString().split(" ");
+      return [month , year];
+    });
+    return monthYear;
+  }
+  toPairs(arr){
+    var reduced = arr.reduce((acc, el) => {
+      !acc[el] ? acc[el] = 1 : acc[el] += 1;
+      return acc
+    },{});
+    let values = Object.values(reduced);
+   return Object.keys(reduced).map((item,ind) => {
+      return [item,values[ind]]
+    });
+  }
+  render(){
+    const { posts } = this.props;
+    return (
+      <aside className="col-md-4">
+        {/* Blog Search Well */}
+        <div className="well">
+          <h4>Search</h4>
+          <form>
+            <div className="input-group">
+              <input type="search" name="search" className="form-control"/>
               <span className="input-group-btn">
                 <button className="btn btn-default" type="submit">
                   <span className="glyphicon glyphicon-search"/>
                 </button>
               </span>
+            </div>
+            {/* /.input-group */}
+          </form>
         </div>
-        {/* /.input-group */}
-      </form>
-    </div>
-    {/* Blog Categories Well */}
-    <div className="well">
-      <h3>Filter Posts</h3>
-      <div className="list-group">
-        <a href="#" className="list-group-item active">
-          <span className="badge">8</span>
-          Show All Posts
-        </a>
-      </div>
-      <h4>
-        <small className="glyphicon glyphicon-tag"/>
-        Category
-      </h4>
-      <div className="list-group">
-        <a href="#" className="list-group-item">
-          <span className="badge">4</span>
-          AngularJS
-        </a>
-        <a href="#" className="list-group-item">
-          <span className="badge">2</span>
-          Grunt
-        </a>
-        <a href="#" className="list-group-item">
-          <span className="badge">6</span>
-          JavaScript
-        </a>
-        <a href="#" className="list-group-item">
-          <span className="badge">2</span>
-          jQuery
-        </a>
-        <a href="#" className="list-group-item">
-          <span className="badge">2</span>
-          Tools
-        </a>
-      </div>
-      <h4>
-        <small className="glyphicon glyphicon-user"/>
-        Author
-      </h4>
-      <div className="list-group">
-        <a href="#" className="list-group-item">
-          <span className="badge">2</span>
-          Alex Ilyaev
-        </a>
-        <a href="#" className="list-group-item">
-          <span className="badge">2</span>
-          Amit Choukroun
-        </a>
-        <a href="#" className="list-group-item">
-          <span className="badge">4</span>
-          Ilan Cohen
-        </a>
-      </div>
-      <h4>
-        <small className="glyphicon glyphicon-time"/>
-        Month
-      </h4>
-      <div className="list-group">
-        <div>
+        {/* Blog Categories Well */}
+        <div className="well">
+          <h3>Filter Posts</h3>
+          <div className="list-group">
+            <Link to="/posts"  activeClassName="active" className="list-group-item active">
+              <span className="badge">{posts.length}</span>
+              Show All Posts
+            </Link>
+          </div>
+          <h4>
+            <small className="glyphicon glyphicon-tag"/>
+            Category
+          </h4>
+          <div className="list-group">
+            {this.toPairs(this.flattenDeep(this.extractArray(posts, 'tags'))).map((item) => {
+              return(
+                <Link key={item[0]} to="/"  activeClassName="active" className="list-group-item">
+                  <span className="badge">{item[1]}</span>
+                  {item[0]}
+                </Link>
+              );
+            })
+            }
+          </div>
+          <h4>
+            <small className="glyphicon glyphicon-user"/>
+            Author
+          </h4>
+          <div className="list-group">
+            {this.toPairs(this.extractArray(posts, 'author')).map((item) => {
+              return(
+                <Link key={item[0]} to="/" activeClassName="active" className="list-group-item">
+                  <span className="badge">{item[1]}</span>
+                  {item[0]}
+                </Link>
+              );
+            })
+            }
+          </div>
+          <h4>
+            <small className="glyphicon glyphicon-time"/>
+            Month
+          </h4>
+          <div className="list-group">
+            {this.toPairs(this.dateToMonthYear(this.extractArray(posts, 'date'))).map((item) => {
+              return(
+                <div key={item[0].slice(item[0].indexOf(',') + 1)}>
               <span className="list-group-item disabled">
-                2015
+                {item[0].slice(item[0].indexOf(',') + 1)}
               </span>
-          <a href="#" className="list-group-item">
-            <span className="badge">4</span>
-            January
-          </a>
+                  <Link to="/"  activeClassName="active" className="list-group-item">
+                    <span className="badge">{item[1]}</span>
+                    {item[0].slice(0, item[0].indexOf(',') )}
+                  </Link>
+                </div>
+              )
+            })
+            }
+          </div>
         </div>
-        <div>
-              <span className="list-group-item disabled">
-                2014
-              </span>
-          <a href="#" className="list-group-item">
-            <span className="badge">4</span>
-            December
-          </a>
-        </div>
-      </div>
-    </div>
-  </aside>
-  );
-};
+      </aside>
+    );
+  }
 
-export default SideBar;
+}
+
+function mapStateToProps(state) {
+  return {
+    posts: state.posts.all
+  }
+}
+
+export default connect(mapStateToProps)(SideBar);
