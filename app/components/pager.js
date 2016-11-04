39724f588dd2d1adb16cde0592c97f615f3ecba5
Noa Fisher <Noa Fisher>
2016-11-04 03:29:59 +0200

    pagination fixed

--- app/components/pager.js
+++ app/components/pager.js
@@ -5,10 +5,10 @@ class Pager extends React.Component {
 
   renderOlderBtn(nextPage, linkToPrev, linkToPrevDecrement) {
     if (nextPage > 2) {
-      return <Link to={linkToPrevDecrement}>← Newer</Link>
+      return <Link to={linkToPrevDecrement}>Newer →</Link>
     }
     else if (nextPage === 2) {
-      return <Link to={linkToPrev}>← Newer</Link>
+      return <Link to={linkToPrev}>Newer →</Link>
     }
     else {
       return null;
@@ -20,13 +20,14 @@ class Pager extends React.Component {
     let linkToNext = queryKey ? {pathname:`/posts/${ nextPage + 1}`, query: query} : `/posts/${ nextPage + 1}`;
     let linkToPrevDecrement = queryKey ? {pathname:`/posts/${ nextPage - 1}`, query: query} : `/posts/${ nextPage - 1}`;
     let linkToPrev = queryKey ? {pathname:'/posts', query: query} : '/posts';
+    console.log(nextPage, query[queryKey], ChunkedVisiblePosts);
     return (
       <ul className="pager">
-        <li className="previous">
+        <li className="next">
           {this.renderOlderBtn(nextPage,linkToPrev, linkToPrevDecrement)}
         </li>
-        { nextPage < ChunkedVisiblePosts ? <li className="next">
-          <Link to={linkToNext}>Older →</Link>
+        { nextPage < ChunkedVisiblePosts ? <li className="previous">
+          <Link to={linkToNext}>← Older</Link>
         </li> : null}
       </ul>
     );
