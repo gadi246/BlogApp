4604799a3eef84654f44e47875cb254ab5ce9dea
Noa Fisher <Noa Fisher>
2016-11-04 19:48:37 +0200

    routing for admin stage

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ import 'assets/css/main.scss';
 import React    from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {Router, useRouterHistory, Route, IndexRedirect, IndexRoute} from 'react-router';
+import {Router, useRouterHistory, Route, IndexRedirect, IndexRoute, Link} from 'react-router';
 import {createHashHistory} from 'history';
 import store from './store';
 
@@ -17,25 +17,50 @@ import SinglePostView from './components/containers/single-post-view';
 const appHistory = useRouterHistory(createHashHistory)({queryKey: false});
 
 
-const Indie = ()=> {
+const AdminContainer = (props)=> {
   return (
-    <h2>List of posts</h2>
+    <div>
+      {props.children}
+    </div>
+  )
+};
+const Admin = ({params})=> {
+  return (
+    <div>
+      <h2>Admin</h2>
+      <h2>side bar</h2>
+      <p>{params.sort}</p>
+      <ul>
+        <li><Link to="/admin/new/post">New</Link></li>
+        <li><Link to="/admin/edit/post/new post">Edit</Link></li>
+      </ul>
+    </div>
   )
 };
 
-const Admina = ()=> {
-  const func = (a, b) => a + b;
+
+const New1 = ()=> {
   return (
-    <h2>Create Post,{func(3, 4)}</h2>
+    <h2>New post here</h2>
   )
 };
+
+const Edit1 = ({params})=> {
+  return (
+    <div>
+      <h2>Edit post</h2>
+      <p>{params.title}</p>
+    </div>
+  )
+};
+
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={appHistory}>
       <Route path="/" component={Root}>
         {/*  Redirect "/" to "/posts"  */}
-      <IndexRedirect to="/posts"/>
+        <IndexRedirect to="/posts"/>
         <Route path="posts" component={PostIndex}>
           <IndexRoute component={PostList}/>
           <Route path=":page" component={PostList}/>
@@ -44,7 +69,12 @@ ReactDOM.render(
         <Route component={PostIndex}>
           <Route path="post/:title" component={SinglePostView}/>
         </Route>
-        <Route path="admin" component={Admina}/>
+        <Route path="admin" component={AdminContainer}>
+          <IndexRoute component={Admin}/>
+          <Route path=":sort" component={Admin}/>
+          <Route path="new/post" component={New1}/>
+          <Route path="edit/post/:title" component={Edit1}/>
+        </Route>
       </Route>
     </Router>
   </Provider>,
